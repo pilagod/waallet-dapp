0f8b3eedeeaa3e22f42dbafc3d9fecb0df91baaa
pilagod <pilagooood@gmail.com>
2024-08-13 12:50:11 +0800

    add request eip712 signature button

--- src/App.tsx
+++ src/App.tsx
@@ -62,7 +62,7 @@ function DApp() {
 }
 
 function Profile() {
-  const { address, chainId } = useAccount();
+  const { address, chainId, connector } = useAccount();
   const { disconnect } = useDisconnect();
   const { data: balance, refetch } = useBalance({
     address,
@@ -138,6 +138,90 @@ function Profile() {
       >
         Transfer
       </button>
+      {connector && (
+        <div>
+          <button
+            onClick={async () => {
+              const provider = (await connector?.getProvider()) as {
+                request(args: { method: string; params: any[] }): any;
+              };
+              await provider.request({
+                method: "eth_signTypedData_v4",
+                params: [
+                  address,
+                  {
+                    types: {
+                      EIP712Domain: [
+                        {
+                          name: "name",
+                          type: "string",
+                        },
+                        {
+                          name: "version",
+                          type: "string",
+                        },
+                        {
+                          name: "chainId",
+                          type: "uint256",
+                        },
+                        {
+                          name: "verifyingContract",
+                          type: "address",
+                        },
+                      ],
+                      Person: [
+                        {
+                          name: "name",
+                          type: "string",
+                        },
+                        {
+                          name: "wallet",
+                          type: "address",
+                        },
+                      ],
+                      Mail: [
+                        {
+                          name: "from",
+                          type: "Person",
+                        },
+                        {
+                          name: "to",
+                          type: "Person",
+                        },
+                        {
+                          name: "contents",
+                          type: "string",
+                        },
+                      ],
+                    },
+                    primaryType: "Mail",
+                    domain: {
+                      name: "Ether Mail",
+                      version: "1",
+                      chainId,
+                      verifyingContract:
+                        "0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC",
+                    },
+                    message: {
+                      from: {
+                        name: "Cow",
+                        wallet: "0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826",
+                      },
+                      to: {
+                        name: "Bob",
+                        wallet: "0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB",
+                      },
+                      contents: "Hello, Bob!",
+                    },
+                  },
+                ],
+              });
+            }}
+          >
+            Request EIP712 signature
+          </button>
+        </div>
+      )}
       {errorMessage && (
         <div style={{ color: "red", width: "350px" }}>{errorMessage}</div>
       )}
